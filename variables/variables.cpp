dc461fd96e93aa1463f137b3f494071aa9051df8
Grasp CS <info@graspcs.com>
2021-03-27 19:03:32 +0530

    Variables

---
+++ variables/variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    short a = 10;
+    int b = 20;
+    long c = 30;
+    long long d = 40;
+    float e = 10.10;
+    double f = 20.20;
+    long double g = 30.30;
+    bool h = true;
+    char i = 'a';
+
+    std::cout << "a = " << a << "\n";
+    std::cout << "b = " << b << "\n";
+    std::cout << "c = " << c << "\n";
+    std::cout << "d = " << d << "\n";
+    std::cout << "e = " << e << "\n";
+    std::cout << "f = " << f << "\n";
+    std::cout << "g = " << g << "\n";
+    std::cout << "h = " << h << "\n";
+    std::cout << "i = " << i << "\n";
+
+    return 0;
+}
